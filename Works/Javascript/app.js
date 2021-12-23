d84b75a7122d6b7ebe1db5bc37ec8210887eaa36
aytajquseynova <aytajquseynova01@gmail.com>
2021-12-23 13:40:01 +0400

    changes

--- Works/Javascript/app.js
+++ Works/Javascript/app.js
@@ -1,1 +1,20 @@
-console.log('salam')
+let a =prompt('a ededini daxil edin')
+let b =prompt('b ededini daxil edin')
+let emr=prompt('istediyiniz riyazi emeliyatin isaresini daxil edin')
+if (emr=='+') {
+    let c= Number(a)+Number(b)
+    alert (c)
+}
+if (emr=='-'){
+    let c= Number(a)-Number(b)
+    alert(c)
+
+}
+if (emr=='/'){
+    let c= Number(a)/Number(b)
+    alert(c)
+}
+if (emr=='*'){
+    let c= Number(a)*Number(b)
+    alert(c)
+}
---
+++ Works/Javascript/exam.js
@@ -0,0 +1,17 @@
+let imtahanbali=70;
+let emr=prompt('imtahan balini daxil edin')
+if (imtahanbali<50) {
+    alert('get deden gelsin')
+    
+} 
+if(imtahanbali>50 && imtahanbali<80){
+   
+    alert('pis deyilsen')
+}
+if(imtahanbali>80 && imtahanbali<100){
+  
+    alert(  'kisiye salam de')
+}
+if(imtahanbali<0 ||imtahanbali>100){
+    alert('daxil etdiyiniz melumat duzgun deyil')
+}
