0c6050b79d5fddaa6154c00a68c1140447f2129d
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-01 17:00:09 +0400

    degiskenleri degistir

---
+++ Works/Javascript/udemy-java/degiskenler/change.js
@@ -0,0 +1,10 @@
+var number1=40;
+var number2=60;
+document.write('birinci ededin deyeri:'+number1+"<br>")
+document.write('ikinci ededin deyeri:'+number2+"<br>")
+var temp='';
+temp=number1;
+number1=number2;
+number2=temp;
+document.write('birinci ededin deyeri:'+number1+"<br>")
+document.write('ikinci ededin deyeri:'+number2+"<br>")
--- Works/Javascript/udemy-java/degiskenler/topla.js
+++ Works/Javascript/udemy-java/degiskenler/topla.js
@@ -3,3 +3,4 @@ var number2=50;
 // indi bunlari toplayaq
 var topla=number1+number2;
 document.write('toplamadan alinan netice:'+topla)
+document.write("<hr>")
