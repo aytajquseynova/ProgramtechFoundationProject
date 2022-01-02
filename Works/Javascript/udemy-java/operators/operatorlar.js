4b4debd96085e3123fefe8dfcb41528f042083d7
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-03 03:07:22 +0400

    operatorlar

---
+++ Works/Javascript/udemy-java/operators/operatorlar.js
@@ -0,0 +1,13 @@
+var a=67;
+var b=23;
+
+var topla=a+b;
+var cix=a-b;
+var vurma=a*b;
+var bolme=a/b;
+var mod=a%b;
+document.write('Toplamanin neticesi:'+topla+"<br>"+
+'Cixmanin neticesi:'+cix+"<br>"+
+ "Vurmanin neticesi:"+vurma+"<br>"+
+"Bolmenin neticesi:"+bolme+"<br>"+
+'Modun neticesi:' +mod)
