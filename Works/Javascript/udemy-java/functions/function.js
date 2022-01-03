e23691d1defba105ac52c3e9d4ef033d44614581
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-03 14:47:37 +0400

    metri santimetre cevirdim

--- Works/Javascript/udemy-java/functions/function.js
+++ Works/Javascript/udemy-java/functions/function.js
@@ -3,3 +3,31 @@
     document.write("xos geldiniz"+" "+ad+" "+soyad)
 }
 userInfo("Aytac","Huseynova")*/
+/*
+function userInfo(ad, soyad){
+    return ad+" "+soyad
+
+}
+document.write(userInfo("Aytac", "Huseynova"));
+*/
+/*
+function vurma(eded1, eded2){
+    return eded1*eded2
+}
+var y=vurma(9,10)
+document.write(y)*/
+
+/*
+function cixma(eded1, eded2){
+    return eded1-eded2
+}
+var z=(9-4)/5
+document.write(z)*/
+
+// metri sm-a  cevirme
+function smetrHesabla(m){
+    var a=m*100;
+    return a;
+}
+var a=smetrHesabla(70)
+document.write(a)
