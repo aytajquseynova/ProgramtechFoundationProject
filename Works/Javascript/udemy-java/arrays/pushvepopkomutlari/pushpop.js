3e644bb1f792324aa84803c0a3bff98a05d14b0a
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-04 13:52:01 +0400

    push ve pop emrleri

---
+++ Works/Javascript/udemy-java/arrays/pushvepopkomutlari/pushpop.js
@@ -0,0 +1,8 @@
+var a=['Aytac', 'Huseynova'];
+a.push('20 yas'); // sona yeni eleman ekler
+document.write(a+"<br>")  
+
+
+var b=['Sebine', 'Huseynova'];
+b.pop('Huseynova'); //pop sondan elemani siler
+document.write(b)
