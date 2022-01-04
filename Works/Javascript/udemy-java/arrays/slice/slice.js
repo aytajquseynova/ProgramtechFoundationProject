1738022048331b38872c202a5126ba7329058d3b
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-04 14:08:23 +0400

    slice ve shift/unshift emrleri

---
+++ Works/Javascript/udemy-java/arrays/slice/slice.js
@@ -0,0 +1,5 @@
+//slice emri mueyyen araliqlardaki elementi gosterir
+var usaqlar=['Aytac','Lale','Hecer','Nicat','Eli','Resad','Togrul']
+var qiz=usaqlar.slice(0,3);
+var oglan=usaqlar.slice(-4)
+document.write(qiz+"<br>"+oglan)
---
+++ Works/Javascript/udemy-java/arrays/unshiftveshiftkomutlari/shift.js
@@ -0,0 +1,6 @@
+var usaqlar=['Aytac', 'Eli'];
+usaqlar.unshift('Sebine'); //basa eleman ekler unshift
+document.write(usaqlar+'<BR>')
+//shift emri ise basdaki elementi silir
+usaqlar.shift('Sebine')
+document.write(usaqlar)
