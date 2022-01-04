a9b628908a414896a25f108be4f6ffeb8f953aef
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-04 14:36:02 +0400

    join

--- Works/Javascript/udemy-java/arrays/join/join.js
+++ Works/Javascript/udemy-java/arrays/join/join.js
@@ -1,3 +1,4 @@
 // join emri ile arrayler birlesib bir string halina gelir
 var yazicilar=['Viktor Huqo',' Dostoyevski','Tolstoy',' Sekspir']
-document.write(yazicilar.join('/'))
+document.write(yazicilar.join('/')+'<br>');
+document.write(yazicilar.join('-'))
