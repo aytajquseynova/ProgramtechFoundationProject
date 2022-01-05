5e8e42dfd792bd7f29d713d23a69ca71a0012268
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-05 07:05:55 +0400

    join method

--- Works/Javascript/javascript-functions/combineword/combineword.js
+++ Works/Javascript/javascript-functions/combineword/combineword.js
@@ -1,5 +1,9 @@
-word1="Aytac";
+/*word1="Aytac";
 word2="Huseynova";
 
 combine=word1.concat(word2)
-document.write(combine)
+document.write(combine)*/
+var fruit = ['apple',  'orange']
+
+result = fruit.join(' ')
+document.write(result)
