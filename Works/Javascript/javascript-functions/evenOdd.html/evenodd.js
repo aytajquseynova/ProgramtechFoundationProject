d88be079a1b1b26161e0c227e035da5710b42511
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-05 06:52:51 +0400

    evenodd

---
+++ Works/Javascript/javascript-functions/evenOdd.html/evenodd.js
@@ -0,0 +1,8 @@
+function evenOrOdd(num) { //for string length take string
+  if (num % 2 == 0) {//and replace numm with string.length here
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+document.write(evenOrOdd(75))
