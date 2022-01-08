2f911cdcbdcb231df5f9fdbd29a8f2527779c0ea
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-08 18:32:38 +0400

    etiket ile erisim

---
+++ Works/Javascript/udemy-java/DOM/etiket.js
@@ -0,0 +1,5 @@
+// etiket ile erisim nasil saglanir
+document.getElementById("baslik").onclick=function(){
+    document.getElementsByTagName("h1")[0].innerHTML='Ust basliq';
+     document.getElementsByTagName("h2")[0].innerHTML='Alt basliq';
+}
