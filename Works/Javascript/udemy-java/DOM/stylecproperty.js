8c927e37df13690dd6e09fc727b7d62b5052af7f
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-08 18:43:59 +0400

    style.property

---
+++ Works/Javascript/udemy-java/DOM/stylecproperty.js
@@ -0,0 +1,7 @@
+// style.property -e baxaq
+document.getElementById('css').onclick=function(){
+
+  document.getElementsByClassName("text")[0].style.color="orange";
+  document.getElementById('content2').style.backgroundColor="red"
+
+}
