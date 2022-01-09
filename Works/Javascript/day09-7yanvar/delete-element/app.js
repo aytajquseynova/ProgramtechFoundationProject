cc10c1a225a941af8c4d1b8314243a5697874b22
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-09 14:56:35 +0400

    element silinir ancaq kliklediyim yox, arasdirim

--- Works/Javascript/day09-7yanvar/delete-element/app.js
+++ Works/Javascript/day09-7yanvar/delete-element/app.js
@@ -2,3 +2,23 @@ function myFunction() {
   var list = document.getElementById("myList");
   list.removeChild(list.childNodes[0]);
 }
+function myFunction1() {
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[1]);
+}
+function myFunction2() {
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[2]);
+}
+function myFunction3() {
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[3]);
+}
+function myFunction4() {
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[4]);
+}
+function myFunction5() {
+  var list = document.getElementById("myList");
+  list.removeChild(list.childNodes[5]);
+}
