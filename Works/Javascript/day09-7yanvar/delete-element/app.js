cd16c5ce4383d1a3c078f92b8cd314e9bf9ce5ed
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-11 02:38:43 +0400

    uzerine klik olunan elementi sil

--- Works/Javascript/day09-7yanvar/delete-element/app.js
+++ Works/Javascript/day09-7yanvar/delete-element/app.js
@@ -1,38 +1,7 @@
-//function myFunction() {
-  //var list = document.getElementById("myList");
-  //list.removeChild(list.childNodes[0]);
-//}
-//*class Siyahi{
-   // constructor(metn)
-  //  {
-       // this.metn=metn;
-   // }
-//}
-///var obj=new Siyahi("Lorem ipsum dolor sit amet 01");
-//document.write(obj.metn())
-function myFunction() {
-  var list = document.getElementById("myList");
-  list.removeChild(list.childNodes[0]);
+/*function myFunction(elem){
+  elem.style.display="none";
+}*/
+function removeSelf(elem){
+  let ul =document.querySelector('ul')
+  ul.removeChild(elem)
 }
-function myFunction1() {
-  var list = document.getElementById("myList");
-  list.removeChild(list.childNodes[1]);
-}
-function myFunction2() {
-  var list = document.getElementById("myList");
-  list.removeChild(list.childNodes[2]);
-}
-function myFunction3() {
-  var list = document.getElementById("myList");
-  list.removeChild(list.childNodes[3]);
-}
-function myFunction4() {
-  var list = document.getElementById("myList");
-  list.removeChild(list.childNodes[4]);
-}
-function myFunction5() {
-  var list = document.getElementById("myList");
-  list.removeChild(list.childNodes[5]);
-}
-
-
