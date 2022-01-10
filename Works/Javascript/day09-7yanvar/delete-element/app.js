e42b4f17493a59050dbac7c4b9abdb4352f08bf5
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-11 02:42:30 +0400

    bu da element silmenin basqa yolu

--- Works/Javascript/day09-7yanvar/delete-element/app.js
+++ Works/Javascript/day09-7yanvar/delete-element/app.js
@@ -1,7 +1,12 @@
 /*function myFunction(elem){
   elem.style.display="none";
 }*/
+/*
 function removeSelf(elem){
   let ul =document.querySelector('ul')
   ul.removeChild(elem)
-}
+}*/
+
+function removeSelf(elem){
+  let ul=elem.parentElement
+ul.removeChild(elem)}
