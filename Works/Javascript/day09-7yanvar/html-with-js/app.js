e2acecc1172dc407ead7328c288ed12fe316e741
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-11 03:29:49 +0400

    ozum practice

--- Works/Javascript/day09-7yanvar/html-with-js/app.js
+++ Works/Javascript/day09-7yanvar/html-with-js/app.js
@@ -1,18 +1,11 @@
-let ul = document.createElement('ul')
+let ul=document.createElement('ul')
 document.body.appendChild(ul)
-for (let i = 0; i < 5; i++) {
-    //let li = document.createElement('li')
-   // ul.appendChild(li)
-   //  li.setAttribute('onclick', 'removeSelf(this)')
-    //li.innerHTML = `Lorem ipsum dolor sit amet 0${i}`
-    // document.body.appendChild(ul)
+for(i=0; i<6;i++){
+    ul.innerHTML+=`<li onclick="removeSelf(this)">Lorem ipsum dolor sit amet 0${i}</li>`
 
-    ul.innerHTML += ` <li onclick="removeSelf(this)">Lorem ipsum dolor sit amet 0${i}</li>`
 }
-
-function removeSelf(elem) {
-    let ul = elem.parentElement
-
+function removeSelf(elem){
+    let ul=elem.parentElement
     ul.removeChild(elem)
 
 }
