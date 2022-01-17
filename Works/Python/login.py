4913d4a3d249f49d3963c706a7ddf7871e52910c
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-17 12:52:14 +0400

    pyhtonla login yazilisi

---
+++ Works/Python/login.py
@@ -0,0 +1,11 @@
+username=input('admin')
+password=input('admin')
+if username=='  ' or password==' ':
+    print('arada bosluq qoymayin')
+if username=='admin' and password=='admin':
+    print('welcome!')
+else:
+    print('melumati duzgun daxil edin!')    
+
+    
+     
