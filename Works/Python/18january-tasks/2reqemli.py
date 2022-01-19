773b8979a6e19c553958bad2fab35c4d6d84a3a2
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-19 06:14:10 +0400

    yaziram amma netice cixartmir

---
+++ Works/Python/18january-tasks/2reqemli.py
@@ -0,0 +1,5 @@
+toplam=0
+for a in range(1,500):
+    if len(str(a))==2:
+       toplam=toplam+a
+print(toplam)
--- Works/Python/login.py
+++ Works/Python/login.py
@@ -2,7 +2,8 @@ username=input('admin')
 password=input('admin')
 if username=='  ' or password==' ':
     print('arada bosluq qoymayin')
-if username=='admin' and password=='admin':
+      else:
+       if username=='admin' and password=='admin':
     print('welcome!')
 else:
     print('melumati duzgun daxil edin!')    
