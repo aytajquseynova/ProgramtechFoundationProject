e51e8fea0bc44dab86fc13cd012098b528a9309b
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-19 07:24:25 +0400

    arrayin butun elementlerin kopyalayaraq ozune elave etdim

---
+++ Works/Python/18january-tasks/doublearr.py
@@ -0,0 +1,7 @@
+arr = ['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+
+b = ['Samir','Mehemmed','Qurbani','Vesile','Qurbaneli','Memmedaga','Nurlan','Leman','Emil','Gulshen']
+
+arr.append(b)
+
+print(arr)
