a705c60bef2c14b9d97b657bf61dcc13375129d3
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-19 14:25:39 +0400

    deyerin eded olub olmadigi da teyin olundu, 17 yanvar tasklari bitdi

---
+++ Works/Python/17january-tasks/numericmethod.py
@@ -0,0 +1,13 @@
+deyer ="6363" #true
+if deyer.isnumeric() == True:  
+    print("ededdir")  
+else:  
+    print("eded deyil")   
+    
+deyer2="aaa"#sehv
+if deyer2.isnumeric()==True:
+ print('ededdir')
+else:
+ print('eded deyil')        
+    
+        
