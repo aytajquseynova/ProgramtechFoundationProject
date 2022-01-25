f6719c59472f53ed876b4f132ae962dac73e9324
aytajquseynova <aytajquseynova01@gmail.com>
2022-01-25 16:32:26 +0400

    app.py`a data elave olundu

--- Works/Python/forpractice/app.py
+++ Works/Python/forpractice/app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app=Flask(__name__)
+detallar=[
+  'Aytac',
+  'Huseynova',
+  '20 yas'
+]
 @app.route('/')
+def home():
+     return render_template('index.html', data=detallar)
+  
+@app.route("/about")
 def page():
-    return('salam dunya')
-if __name__=="__main__":
+  return render_template('about.html')
+
+if __name__=='__main__':
     app.run(debug=True)
