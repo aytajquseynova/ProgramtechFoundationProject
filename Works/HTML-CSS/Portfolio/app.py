d20329eedfe8997c1f5abea3651623f72af1e4af
aytajquseynova <aytajquseynova01@gmail.com>
2022-02-10 12:11:14 +0400

    qovluq strukturu deyisdirildi

--- Works/HTML-CSS/Portfolio/app.py
+++ Works/HTML-CSS/Portfolio/app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
 import random
+import os
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new.db'
+db = SQLAlchemy(app)
 #about sectionu baslayir
 class About:
     def __init__(self,id,name,shortInfo):
@@ -8,9 +14,9 @@ class About:
         self.shortInfo=shortInfo
 about=[
      About(
-                random.randint(1,500),
-                'Mən, Aytac Hüseynova', 
-                'Salam, mən Aytac Hüseynova. 20 yaşım var. Azərbaycan Dövlət Neft və Sənaye Universiteti SABAH fakültəsi Proseslərin avtomatlaşdırılması mühəndisliyində 4-cü kurs tələbəsiyəm. Eyni zamanda "Pragmatech Education" kursu vasitəsilə development sahəsi üzrə təhsil alıram. Məqsədim Front-End Developer olmaqdır.')
+        random.randint(1,500),
+        'Mən, Aytac Hüseynova', 
+        'Salam, mən Aytac Hüseynova. 20 yaşım var. Azərbaycan Dövlət Neft və Sənaye Universiteti SABAH fakültəsi Proseslərin avtomatlaşdırılması mühəndisliyində 4-cü kurs tələbəsiyəm. Eyni zamanda "Pragmatech Education" kursu vasitəsilə development sahəsi üzrə təhsil alıram. Məqsədim Front-End Developer olmaqdır.')
         ]
 #about sectionu bitir
 #skills sectionu baslayir
@@ -133,43 +139,53 @@ portfolio=[
 ] 
 #portfolio sectionu bitir
 #references sectionu baslayir 
-class References:
-    def __init__(self,id, rey,img,cmntrname, cmntrjob):
-        self.id=id
-        self.rey=rey
-        self.img=img
-        self.cmntrname=cmntrname
-        self.cmntrjob=cmntrjob
-references=[
-    References(
-        random.randint(1,500),
-       '"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit mlaborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! Npossimus illo architecto praesentium ut aliquam dolorem."',
-       "http://themeelite.com/demos/flatr/img/referance1@2x.jpg",
-       "Gulsen Zalova",
-       'Front-End Developer'
-       ),
-     References(
-        random.randint(1,500),
-       '"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit m   laborum sequi, magni earum quo soluta sint velit numquam, ipsum illum! N possimus illo architecto praesentium ut aliquam dolorem."',
-       "http://themeelite.com/demos/flatr/img/referance2@2x.jpg",
-       "Emil Abdullayev",
-       'Back-End Developer'
-       ),
-     
-]     
+
+# Feedback
+class Feedbacks(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    feedback_img = db.Column(db.String(100))
+    feedback_rey = db.Column(db.String(100))
+    feedback_cmntrname = db.Column(db.String(100))
+    feedback_cmntrjob = db.Column(db.String(100))
+
+@app.route("/admin/feedbacks",methods=["GET","POST"])
+def feedback():
+    from werkzeug.utils import secure_filename
+    feedbacks = Feedbacks.query.all()
+    if request.method=="POST":
+        file = request.files['img']
+        filename=file.filename
+        file.save(os.path.join('static/uploads/',filename))
+        feedback_rey = request.form["rey"]
+        feedback_cmntrname=request.form["cmntrname"]
+        feedback_cmntrjob=request.form["cmntrjob"]
+        print(feedback_rey)
+        prjct = Feedbacks(
+            feedback_rey = feedback_rey,
+            feedback_img = filename,
+            feedback_cmntrname=feedback_cmntrname,
+            feedback_cmntrjob=feedback_cmntrjob,
+        )
+        db.session.add(prjct)
+        db.session.commit()
+        return redirect("/")
+    return render_template('admin/feedbacks.html',feedbacks=feedbacks)
+
+
+
 #references sectionu bitir   
-app=Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("app/index.html",about=about,skill=skill,work=work,education=education,portfolio=portfolio,references=references )
+    feed = Feedbacks.query.all()
+    return render_template("app/index.html", feed=feed)
 
 @app.route("/admin")
 def admin():
     return render_template('admin/base.html')
 
-@app.route("/admin/feedbacks")
-def feedback():
-    return render_template('admin/feedbacks.html')
+@app.route("/admin/skill")
+def skill():
+    return render_template('admin/skill.html')
 
 @app.route("/admin/tehsil")
 def tehsil():
@@ -179,4 +195,5 @@ def tecrube():
     return render_template('admin/tecrube.html')
 
 if __name__=="__main__":
+    # db.create_all()
     app.run(debug=True)
